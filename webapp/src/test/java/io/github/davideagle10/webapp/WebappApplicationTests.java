220fc49a70566391e6bbb999e57d0696eb9513f2
David <davidaguilarsg10@gmail.com>
2026-01-15 20:31:05 -0500

    initial commit

---
+++ webapp/src/test/java/io/github/davideagle10/webapp/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.davideagle10.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
